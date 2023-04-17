1f15c9b5c3a7563e7cb967605fd577b7787da5d9
Sajal Kumar <92041223+Sajal133@users.noreply.github.com>
2023-04-17 11:20:11 +0530

    Added solution - LeetHub

--- Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.java
+++ Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.java
@@ -43,17 +43,9 @@ class Solution{
     //Function to count the frequency of all elements from 1 to N in the array.
     public static void frequencyCount(int arr[], int N, int P)
     {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<N;i++)
-        {
-            if(map.containsKey(arr[i]))
-            {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            }
-            else
-            {
-                map.put(arr[i], 1);
-            }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
         for(int i = 1;i<=N;i++)
         {
