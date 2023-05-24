b0ad947a6565b61a1c778d56387327661a7c348f
Sajal Kumar <92041223+Sajal133@users.noreply.github.com>
2023-05-24 22:33:44 +0530

    Added solution - LeetHub

---
+++ Doubly linked list Insertion at given position - GFG/doubly-linked-list-insertion-at-given-position.java
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}
+
+class DLinkedList
+{
+	
+	Node newNode(Node head, int data)
+	{
+		Node n = new Node(data);
+		if(head == null)
+		{
+		    head = n;
+		    return head;
+		}
+		head.next = n;
+		n.prev = head;
+		head = n;
+		return head;
+	}
+	
+	
+	void printList(Node node)
+	{
+		Node temp = node;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		
+		while(temp.prev != null)
+		{
+			temp = temp.prev;
+		}
+		
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		DLinkedList DLL = new DLinkedList();
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			Node temp;
+			Node head = null;
+			Node root = null;
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++)
+			{
+				int x = sc.nextInt();
+				head = DLL.newNode(head,x);
+			    if(root == null)    root = head;
+			}
+			head = root;
+			int pos = sc.nextInt();
+			int data = sc.nextInt();
+			
+		    GfG g = new GfG();
+		    g.addNode(head,pos,data);
+			
+			DLL.printList(head);
+			while(head.next != null)
+			{
+				temp = head;
+				head = head.next;
+			}
+		    t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+
+class GfG {
+    void addNode(Node head, int p, int x) {
+        Node newNode = new Node(x);
+
+        if (head == null) {
+            // If the list is empty, make the new node as the head
+            head = newNode;
+        } else {
+            Node current = head;
+            int count = 0;
+
+            // Traverse the list to locate the pth node
+            while (current.next != null && count < p) {
+                current = current.next;
+                count++;
+            }
+
+            // Adjust the pointers to insert the new node
+            newNode.next = current.next;
+            newNode.prev = current;
+            if (current.next != null) {
+                current.next.prev = newNode;
+            }
+            current.next = newNode;
+        }
+    }
+}
