125365da70610eb748c4f3b9440ba6ffd90f0517
Sajal Kumar <92041223+Sajal133@users.noreply.github.com>
2023-05-25 20:36:10 +0530

    Added solution - LeetHub

---
+++ Delete node in Doubly Linked List - GFG/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int d)
+	{
+		data = d;
+		next = prev = null;
+	}
+}
+class Delete_Node_From_DLL
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node)
+	{
+		if(head ==  null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail.next.prev = tail;
+			tail = tail.next;
+		}
+	}
+	
+	void printList(Node head)
+	{	Node temp = head;
+		while(temp != null)
+		{
+			System.out.print(temp.data+" ");
+			temp =  temp.next;
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			Delete_Node_From_DLL list = new Delete_Node_From_DLL();
+			
+			int a1 = sc.nextInt();
+			Node head = new Node(a1);
+			list.addToTheLast(head);
+			
+			for(int i=1;i<n;i++)
+			{
+				int a = sc.nextInt();
+				list.addToTheLast(new Node(a));
+			}
+			a1 = sc.nextInt();
+			Solution g = new Solution();
+			//System.out.println(list.temp.data);
+			Node ptr = g.deleteNode(list.head, a1);
+			list.printList(ptr);
+			t--;
+		}
+	}
+}
+
+
+                               
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Structure of linkedlist node
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int d)
+	{
+		data = d;
+		next = prev = null;
+	}
+}
+*/
+class Solution {
+    // function returns the head of the linkedlist
+    Node deleteNode(Node head, int position) {
+        if (head == null) {
+            // If the list is empty, there's nothing to delete
+            return null;
+        }
+
+        if (position == 1) {
+            // If the head node is the one to be deleted, update the head reference
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+            return head;
+        }
+
+        Node current = head;
+        int count = 1;
+
+        // Traverse the list to locate the node to be deleted at the given position
+        while (current != null && count < position) {
+            current = current.next;
+            count++;
+        }
+
+        if (current == null) {
+            // Node at the given position was not found
+            return head;
+        }
+
+        // Adjust the pointers to remove the node
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        }
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        }
+
+        return head;
+    }
+}
