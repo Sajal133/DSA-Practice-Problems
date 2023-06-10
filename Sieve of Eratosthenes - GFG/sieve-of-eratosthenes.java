e956c64a657198ae057bcda94938cd0d82ccac94
Sajal Kumar <92041223+Sajal133@users.noreply.github.com>
2023-06-10 11:11:54 +0530

    Added solution - LeetHub

---
+++ Sieve of Eratosthenes - GFG/sieve-of-eratosthenes.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            Solution ob = new Solution();
+            ArrayList<Integer> primes  = ob.sieveOfEratosthenes(N);
+            for(int prime : primes) {
+                System.out.print(prime+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static ArrayList<Integer> sieveOfEratosthenes(int n){
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+  
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (prime[i] == true)
+                res.add(i);
+        }
+        return res;
+    }
+}
+  
